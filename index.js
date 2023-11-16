df8ba3afc7c382994cf32d54f50eceaf3a9a2bbd
Rogelio <roger.cruz808@gmail.com>
2023-11-16 13:32:53 -0800

    finished stoplight

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
-(function() {
-  'use strict';
+// Stoplight exercise part 1
 
-  // YOUR CODE HERE
-})();
+// button variables
+let stopButton = document.getElementById("stopButton")
+let slowButton = document.getElementById("slowButton")
+let goButton = document.getElementById("goButton")
+
+
+// Function turning off and on each class type
+var lightFunction = function(typeOfLight, classType) {
+  var element = document.getElementById(typeOfLight);
+  element.classList.toggle(classType);
+}
+
+// Creating an event listener for each button 
+stopButton.addEventListener('click', function (){
+  lightFunction("stopLight", "stop");
+});
+slowButton.addEventListener('click', function (){
+  lightFunction("slowLight", "slow");
+});
+goButton.addEventListener('click', function (){
+  lightFunction("goLight", "go")
+});
+
+
+
