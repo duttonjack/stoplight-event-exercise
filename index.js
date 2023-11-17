a2b73d9ab58fd7a159880b0a3f2946b842087725
Jack Dutton <duttonjack@Jacks-MacBook-Pro.local>
2023-11-16 21:23:56 -0500

    Part 2 implemented solo

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ let goLight = document.getElementById('goLight');
 var toggleStop = function(){
   stopLight.classList.toggle("stop")
 }
-debugger;
+
 var toggleSlow = function(){
   console.log('slow')
   slowLight.classList.toggle("slow")
@@ -26,4 +26,25 @@ var toggleGo = function(){
 stopButton.addEventListener('click', toggleStop);
 slowButton.addEventListener('click', toggleSlow);
 goButton.addEventListener('click', toggleGo);
+
+// Part 2 - Log message when user's mouse enters or leaves button area
+
+var enterLog = function(event){
+  let text = event.target.innerText
+  console.log(`Entered ${text} button`)
+}
+
+var exitLog = function(event){
+  let text = event.target.innerText
+  console.log(`Left ${text} button`)
+}
+
+stopButton.addEventListener('mouseenter', enterLog);
+slowButton.addEventListener('mouseenter', enterLog);
+goButton.addEventListener('mouseenter', enterLog);
+
+stopButton.addEventListener('mouseleave', exitLog);
+slowButton.addEventListener('mouseleave', exitLog);
+goButton.addEventListener('mouseleave', exitLog);
+
 
